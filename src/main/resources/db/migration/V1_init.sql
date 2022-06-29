96848dab6ca5ada64bc93570c70c37d788362a7d
celean77777 <celean77777@gmail.com>
2022-06-29 21:07:21 +0300

    Initial commit

---
+++ src/main/resources/db/migration/V1_init.sql
@@ -0,0 +1,38 @@
+create table users
+(
+    id       bigserial,
+    username varchar(30) not null unique,
+    password varchar(80) not null,
+    email    varchar(50) unique,
+    primary key (id)
+);
+
+create table roles
+(
+    id   serial,
+    name varchar(50) not null,
+    primary key (id)
+);
+
+CREATE TABLE users_roles
+(
+    user_id bigint not null,
+    role_id int    not null,
+    primary key (user_id, role_id),
+    foreign key (user_id) references users (id),
+    foreign key (role_id) references roles (id)
+);
+
+insert into roles (name)
+values ('ROLE_USER'),
+       ('ROLE_ADMIN');
+
+insert into users (username, password, email)
+values
+    ('user1', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'user1@gmail.com'),
+    ('user2', '$2a$15$Kx7V7zB6yPJfzTIfFb.ug.WHHBdajwhSoZSBUtz1cBOmm0B0btIfe', 'user2@gmail.com'),
+    ('user3', '$2a$15$C6IZ7Nu7Bo6VkbWy2xdLE.2ZrFl/Izh22B9vbUcf00mXxeNvNbcKO', 'user3@gmail.com');
+
+insert into users_roles (user_id, role_id)
+values
+    (1, 1) ;
